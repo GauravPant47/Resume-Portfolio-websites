eff08bf8e27411f858c35c8463b1c970c861adde
Gaurav <gaurav556pant@gmail.com>
2023-05-09 23:11:21 +0530

    Adding mailServer and application.propoties

---
+++ resume/src/main/java/net/resume/service/MailService.java
@@ -0,0 +1,7 @@
+package net.resume.service;
+
+import net.resume.model.Mail;
+
+public interface MailService {
+	public void sendEmail(Mail mail);
+}
---
+++ resume/src/main/java/net/resume/service/MailServiceImpl.java
@@ -0,0 +1,33 @@
+package net.resume.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import net.resume.model.Mail;
+
+public class MailServiceImpl implements MailService {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Override
+	public void sendEmail(Mail mail) {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		try {
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+			mimeMessageHelper.setSubject(mail.getMailSubject());
+			mimeMessageHelper.setFrom(new InternetAddress(mail.getMailForm()));
+			mimeMessageHelper.setTo(mail.getMailTo());
+			mimeMessageHelper.setText(mail.getMailContent());
+			javaMailSender.send(mimeMessageHelper.getMimeMessage());
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
